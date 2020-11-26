fafd938ee1de6d4f33224bcda9034bfbcb50eed6
rifki ahmad sururi <57984062+rifkisururi@users.noreply.github.com>
2020-11-26 12:11:33 +0700

    first commit

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -11,41 +11,43 @@ use Illuminate\Support\Facades\Session;
 class DashboardController extends Controller
 {
 
-	public function index(){
-		
-		return view('admin.dashboard.index');
+	public function index()
+	{
 
+		return view('admin.dashboard.index');
 	}
 
-	public function laporan(Request $request){
+	public function laporan(Request $request)
+	{
 
 		$bulan = $request->get('bulan');
 		$tahun = $request->get('tahun');
 
 		$data = DB::table('pemesanan')
-				->join('kamar','pemesanan.kamar_id','kamar.id')
-				->join('user','pemesanan.user_id','user.id')
-				->whereMonth('check_in', $bulan)
-				->whereYear('check_in', $tahun)
-				->select('pemesanan.*','kamar.nomor','user.nama')
-				->get();
+			->join('kamar', 'pemesanan.kamar_id', 'kamar.id')
+			->join('user', 'pemesanan.user_id', 'user.id')
+			->whereMonth('check_in', $bulan)
+			->whereYear('check_in', $tahun)
+			->where('status', 'sudah dibayar')
+			->select('pemesanan.*', 'kamar.nomor', 'user.nama')
+			->get();
 
 		$jumlah_pemasukan = DB::table('pemesanan')
-						->whereMonth('check_in', $bulan)
-						->whereYear('check_in', $tahun)
-						->sum('harga');
+			->whereMonth('check_in', $bulan)
+			->whereYear('check_in', $tahun)
+			->where('status', 'sudah dibayar')
+			->sum('harga');
 
 		$jumlah_pemesanan = DB::table('pemesanan')
-							->whereMonth('check_in', $bulan)
-							->whereYear('check_in', $tahun)
-							->count();
+			->whereMonth('check_in', $bulan)
+			->whereYear('check_in', $tahun)
+			->where('status', 'sudah dibayar')
+			->count();
 
 		$callback['data'] = $data;
 		$callback['jumlah_pemasukan'] = $jumlah_pemasukan;
 		$callback['jumlah_pemesanan'] = $jumlah_pemesanan;
 
 		return response()->json($callback);
-
 	}
-
 }
